698e3a788814b529f09cb5c25606ce596dcbc648
struk49 <andrewstruk@yahoo.co.uk>
2023-03-29 09:36:49 +0000

    set structure of game functions

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function  calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswers() {
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
