fd9399b32c84551c808b174aa6f7564b88791f16
struk49 <andrewstruk@yahoo.co.uk>
2023-03-30 05:47:59 +0000

    added code to generate addition question fo game

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,10 +10,11 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+    runGame('addition');
 });
 
 
@@ -21,11 +22,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game loop, called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     //creates two numbers between 1 and 25
     let num1 = Math.floor(Math.random()*25)+1;
     let num2 = Math.floor(Math.random()*25)+1;
-}
+
+    if(gameType === 'addition'){
+        displayAdditionQuestion(num1, num2);
+    }else {
+    alert('unkown game type: ${gameType}');
+    throw 'unkown game type: ${gameType}. aborting';
+    }
+ }
+
 
 function checkAnswer() {
 }
@@ -42,7 +51,10 @@ function incrementWrongAnswers() {
 
 }
 
-function displayAdditionQuestion(){
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent = operand1;
+     document.getElementById('operand2').textContent = operand2;
+      document.getElementById('operator').textContent = "+";
 
 }
 
