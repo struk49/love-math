b628f0a82ebcc081990eb84b82ae5b33676398d8
struk49 <andrewstruk@yahoo.co.uk>
2023-03-30 10:18:49 +0000

    add code for multiply game

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,6 +29,9 @@ function runGame(gameType) {
 
     if(gameType === 'addition'){
         displayAdditionQuestion(num1, num2);
+    }else if(gameType === 'multiply'){
+        displayMultiplyQuestion(num1, num2);
+    
     }else {
     alert(`unkown game type: ${gameType}`);
     throw (`unkown game type: ${gameType}. aborting`);
@@ -65,7 +68,9 @@ function  calculateCorrectAnswer() {
 
     if(operator === '+'){
         return[operand1 + operand2,"addition"];
-    }else {
+    }else if(operator === 'x'){
+        return [operand1 * operand2, "multiply"];
+    }else{
         alert(`unimplemented operator ${operator}`);
         throw (`unimplemented operator ${operator}. Aborting`);
     }
@@ -101,6 +106,9 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+     document.getElementById('operand2').textContent = operand2;
+      document.getElementById('operator').textContent = "x";
 
 }
